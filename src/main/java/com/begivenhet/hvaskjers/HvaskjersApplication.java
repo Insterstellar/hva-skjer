606a271454afab458b5c9e6abb474167ba6af467
Mandy <mandyronald58@gmail.com>
2023-05-30 22:49:08 +0200

    intial commit

--- src/main/java/com/begivenhet/hvaskjers/HvaskjersApplication.java
+++ src/main/java/com/begivenhet/hvaskjers/HvaskjersApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/welcome")
+@RequestMapping("")
 public class HvaskjersApplication {
 
 	public static void main(String[] args) {
